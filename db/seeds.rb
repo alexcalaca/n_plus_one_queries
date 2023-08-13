d2bee2f6cd3d723dac36477ed13fab3a3b550362
Alexandre Calaça <alexcalaca@gmail.com>
2023-08-13 01:58:52 -0300

    feat: update sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-post1 = Post.create(title: 'Sample Post 1')
-100.times { |i| post1.comments.create(body: "Comment #{i + 1} - Sample Post 1") }
-
-post2 = Post.create(title: 'Sample Post 2')
-100.times { |i| post2.comments.create(body: "Comment #{i + 1} - Sample Post 2") }
+10.times do |i|
+  post = Post.create(title: "Sample Post #{i + 1}")
+  20.times { |j| post.comments.create(body: "Comment #{j + 1} - Sample Post #{i + 1}") }
+end
